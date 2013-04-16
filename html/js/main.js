714018e682fa8aab59fa8534309dbd507c5a09a3
Steven Birkner <stevebirk36@gmail.com>
2013-04-16 13:31:35 -0400

    redoing the header

--- html/js/main.js
+++ html/js/main.js
@@ -1,1 +1,18 @@
+function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
+
+/*
+$("#lhome").click(function() {
+   scrollToAnchor('home');
+});
+$("#lcreators").click(function() {
+   scrollToAnchor('creators');
+});
+$("#games").click(function() {
+   scrollToAnchor('games');
+});
+*/
+
 

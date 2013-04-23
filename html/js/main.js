db43683d860d6b32dbf1e66befdc5c9166dc16a8
Steven Birkner <stevebirk36@gmail.com>
2013-04-23 16:10:13 -0400

    js slide thing works still need to fig out smooth scroll.

--- html/js/main.js
+++ html/js/main.js
@@ -1,27 +1,7 @@
-function scrollToAnchor(aid){
-    var aTag = $("a[name='"+ aid +"']");
-    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
-}
+$("a.first").pageslide();
 
-$("#lhome").click(function() {
-   scrollToAnchor('home');
-});
 
-$("#lcre").click(function() {
-   scrollToAnchor('creators');
-});
 
-$("#lcomp").click(function() {
-   scrollToAnchor('companies');
-});
-
-$("#lgames").click(function() {
-   scrollToAnchor('games');
-});
-
-$("#labout").click(function() {
-   scrollToAnchor('aboutus');
-});
 
 
 

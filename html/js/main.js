e438c1bc58c9a54aa65cf7edf5de0615bdbbedc7
Steven Birkner <stevebirk36@gmail.com>
2013-05-07 18:31:34 -0400

    done addig in pictures

--- html/js/main.js
+++ html/js/main.js
@@ -1,30 +1,17 @@
-
-
-
 $(document).ready(function(){
     
-    
+    $('a[href^="#"]').click(function(event) {
+
+        
+        var id = $(this).attr("href");
+        var offset = 1;
+        var target = $(id).offset().top - offset;
+        $('html, body').animate({scrollTop:target}, 500);
+        event.preventDefault();
+    });
     
   
     
     
         
 });
-
-
-
-
-//Scroll
-$('a[href^="#"]').click(function(event) {
-
-        
-        var id = $(this).attr("href");
-        var offset = 5;
-        var target = $(id).offset().top - offset;
-        $('html, body').animate({scrollTop:target}, 500);
-        event.preventDefault();
-    });
-
-
-//content additoin
-
